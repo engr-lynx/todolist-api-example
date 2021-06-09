5fca0c8dc8e0bdeba9e757d9d187b33390222c3d
Leon Valencia <techie@lynx.rocks>
2021-06-09 12:09:25 +0800

    allow CORS

--- app/index.js
+++ app/index.js
@@ -10,7 +10,12 @@ import TodoCollection from './models/TodoCollection'
 const server = Hapi.server({
   host: process.env.SERVER_HOST || '0.0.0.0',
   port: process.env.SERVER_PORT || '8000',
-  routes: { cors: true }, // So that localhost from different ports can connect.
+  routes: {
+    cors: {
+      origin: ['*'],
+      headers: ['Authorization', 'Origin', 'X-Requested-With', 'Content-Type', 'Accept'],
+    }
+  }, // So that localhost from different ports can connect.
   debug: { request: ['error'] } // Always log errors.
 })
 
