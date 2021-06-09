bf5bf1f646ff350ece69dfbffa19fa5db03a10f9
Leon Valencia <techie@lynx.rocks>
2021-06-09 10:13:55 +0800

    fix connecting to DocumentDB

--- app/database.js
+++ app/database.js
@@ -1,11 +1,18 @@
 import mongoose from 'mongoose'
+import readFileSync from 'fs'
 
 export default async () => {
   try {
     const connectionStr = process.env.MONGODB_CONNECTION_STRING
+    const ca = [readFileSync('rds-combined-ca-bundle.pem')]
 
     console.log('MONGODB:', connectionStr)
-    await mongoose.connect(connectionStr, { useNewUrlParser: true })
+    console.log(ca)
+    await mongoose.connect(connectionStr, {
+      sslValidate: true,
+      sslCA:ca,
+      useNewUrlParser: true,
+    })
 
     console.log('Connection with database succeeded')
     return mongoose.connection
